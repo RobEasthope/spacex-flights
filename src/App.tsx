0b63aaa5b77adba5e64d7aea67a40407b743387d
Rob Easthope <hello@robeasthope.com>
2019-01-11 12:35:20 +0000

    Swap around functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
+import { ConnectedRouter } from "connected-react-router";
+import { createBrowserHistory } from "history";
 import React from "react";
+import { Provider } from "react-redux";
+
+import configureStore from "./configureStore";
+
+const history = createBrowserHistory();
+
+const initialState = window.initialReduxState;
+const store = configureStore(history, initialState);
 
 import { Normalize } from "styled-normalize";
 import GlobalStyles from "./styles/GlobalStyles";
@@ -6,11 +16,15 @@ import Routes from "./routes";
 
 const App = () => {
   return (
-    <React.Fragment>
-      <Normalize />
-      <GlobalStyles />
-      <Routes />
-    </React.Fragment>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <React.Fragment>
+          <Normalize />
+          <GlobalStyles />
+          <Routes />
+        </React.Fragment>
+      </ConnectedRouter>
+    </Provider>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,11 @@
-import { ConnectedRouter } from "connected-react-router";
-import { createBrowserHistory } from "history";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 
-import configureStore from "./configureStore";
 import * as serviceWorker from "./serviceWorker";
 
 import App from "./App";
 
-const history = createBrowserHistory();
-
-const initialState = window.initialReduxState;
-const store = configureStore(history, initialState);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
