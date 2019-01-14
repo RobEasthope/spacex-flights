963de2b52489d9056daa109cab9327538c904928
Rob Easthope <hello@robeasthope.com>
2019-01-14 11:29:45 +0000

    Tweak markup

--- src/pages/HoldingPage/HoldingPage.tsx
+++ src/pages/HoldingPage/HoldingPage.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 const HoldingPage = () => (
-  <div>
+  <main>
     <Helmet>
       <meta charSet="utf-8" />
       <title>My Title</title>
       <link rel="canonical" href="http://mysite.com/example" />
     </Helmet>
     <h1>SpaceX Flights</h1>
-  </div>
+  </main>
 );
 
 export default HoldingPage;
