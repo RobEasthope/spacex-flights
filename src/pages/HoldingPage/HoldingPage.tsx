1e653ec70a40f77d4dc475354f55a6d07ded8415
Rob Easthope <hello@robeasthope.com>
2019-01-14 11:29:15 +0000

    Add Helmet component

--- src/pages/HoldingPage/HoldingPage.tsx
+++ src/pages/HoldingPage/HoldingPage.tsx
@@ -2,6 +2,11 @@ import React from "react";
 
 const HoldingPage = () => (
   <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>My Title</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+    </Helmet>
     <h1>SpaceX Flights</h1>
   </div>
 );
