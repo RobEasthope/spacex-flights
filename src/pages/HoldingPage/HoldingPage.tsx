5c92f62dbc9a3b5f9c97f648e236c9a3f7986e83
Rob Easthope <hello@robeasthope.com>
2019-01-14 11:37:07 +0000

    Add open graph & twitter meta data

--- src/pages/HoldingPage/HoldingPage.tsx
+++ src/pages/HoldingPage/HoldingPage.tsx
@@ -5,6 +5,27 @@ const HoldingPage = () => (
     <Helmet>
       <title>SpaceX Flights</title>
       <link rel="canonical" href="https://www.spacexflights.io/" />
+      <meta
+        property="description"
+        content="The latest information on SpaceX launches, rockets and vehicles"
+      />
+
+      {/* Open Graph meta data */}
+      <meta property="og:title" content="SpaceX Flights" />
+      <meta property="og:url" content={`https://www.spacexflights.io/`} />
+      <meta property="og:site_name" content="SpaceX Flights" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:description"
+        content="The latest information on SpaceX launches, rockets and vehicles"
+      />
+      <meta property="og:image" content="" />
+      <meta property="og:locale" content="en_GB" />
+
+      {/* Twitter meta data */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="Space X Flights" />
+      <meta name="twitter:creator" content="@robeasthope" />
     </Helmet>
 
     <main>
